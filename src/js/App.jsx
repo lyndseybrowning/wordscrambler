43e455fa9dfbc23e80aea7c0a96cb0219d10c724
Lyndsey Browning <lbrowning86@gmail.com>
2018-04-12 17:20:44 +0100

    Create basic App component and reformat indentation

---
+++ src/js/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
+
+export default App;
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,59 +1,64 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
-const SRC_PATH = "./src";
-const HTML_ENTRY = "index.html";
-const entry = `${SRC_PATH}/js/index.js`;
+const SRC_PATH = './src';
+const HTML_ENTRY = 'index.html';
+const entry = `${SRC_PATH}/js/index.jsx`;
 const env = process.env.NODE_ENV;
-const production = env === "production";
+const production = env === 'production';
 
 const config = {
-    entry,
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
+  entry,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+        resolve: {
+          extensions: ['.js', '.jsx'],
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: production },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
             },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader",
-                        options: { minimize: production }
-                    }
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader, "css-loader", {
-                        loader: "sass-loader",
-                        options: {
-                            sourceMap: true
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: `${SRC_PATH}/${HTML_ENTRY}`,
-            filename: `./${HTML_ENTRY}`
-        }),
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-            chunkFilename: "[id].css"
-        })
-    ]
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${SRC_PATH}/${HTML_ENTRY}`,
+      filename: `./${HTML_ENTRY}`,
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
 };
 
 if (!production) {
-    config.devtool = "source-map";
+  config.devtool = 'source-map';
 }
 
 export default config;
