4f7546bf8fa82b208bb23fe0b853d43470eb4527
Lyndsey Browning <lbrowning86@gmail.com>
2018-05-18 17:05:44 +0100

    Create site wrapper for footer image

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -4,12 +4,12 @@ import logo from '../img/logo.svg';
 const App = () => (
   <Fragment>
     <header className="c-header">
-      <div className="o-site-wrap">
+      <div className="o-wrapper">
         <img src={logo} alt="Word Scrambler Logo" aria-roledescription="logo" />
       </div>
     </header>
     <main>
-      <div className="o-site-wrap">
+      <div className="o-wrapper">
         Main content area
       </div>
     </main>
