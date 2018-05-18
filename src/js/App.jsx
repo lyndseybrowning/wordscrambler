ed6d7e37c62f214e8ec836711ffea0ead090e7dd
Lyndsey Browning <lbrowning86@gmail.com>
2018-05-18 17:40:04 +0100

    Add react-router-dom and create initial screen

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,20 +1,28 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import logo from '../img/logo.svg';
 
 const App = () => (
-  <Fragment>
-    <header className="c-header">
-      <div className="o-wrapper">
-        <img src={logo} alt="Word Scrambler Logo" aria-roledescription="logo" />
-      </div>
-    </header>
-    <main>
-      <div className="o-wrapper">
-        Main content area
-      </div>
-    </main>
-    <footer className="c-footer" />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <header className="c-header">
+        <div className="o-wrapper">
+          <img src={logo} alt="Word Scrambler Logo" aria-roledescription="logo" />
+        </div>
+      </header>
+      <main>
+        <div className="o-wrapper">
+          <h1>How to Play</h1>
+          <p>
+            Use your mouse or keyboard to find as many words as you can before the timer runs out!
+          </p>
+          <p>Ready to play? Click the button below.</p>
+          <button type="button" className="c-button">Start Game</button>
+        </div>
+      </main>
+      <footer className="c-footer" />
+    </Fragment>
+  </Router>
 );
 
 export default App;
